48fa0430dde2e8569dd395206571b742096f2b4d
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-07-31 10:04:05 -0300

    Hero crossgeo

--- src/lib/components/landing-page/hero/Hero.svelte
+++ src/lib/components/landing-page/hero/Hero.svelte
@@ -1,16 +1,19 @@
 <script>
   import * as m from '$msgs'
   let image_url = 'https://via.placeholder.com/672x448'
+
+  import StaggeredLetterPullUpAnimation from '$lib/components/animation/StaggeredLetterPullUpAnimation.svelte'
+  import FlipText from '$lib/components/animation/FlipText.svelte';
 </script>
 
-<div
-  class="hero min-h-screen"
-  style="background-image: url({image_url});"
->
+<div class="hero min-h-screen" style="background-image: url({image_url});">
   <div class="hero-overlay bg-opacity-60"></div>
   <div class="hero-content text-center text-neutral-content">
     <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">{m.hero_title()}</h1>
+      <h1 class="mb-5 text-5xl font-bold">
+        <!-- {m.hero_title()} -->
+        <FlipText words={m.hero_title()}  />
+      </h1>
       <p class="mb-5">
         {m.hero_description()}
       </p>

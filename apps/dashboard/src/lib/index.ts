de5af3c5a5c43ef55e6fdb2ac62d932d83c04f8f
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-07-13 21:05:34 -0300

    Update index.ts

--- apps/dashboard/src/lib/index.ts
+++ apps/dashboard/src/lib/index.ts
@@ -1,4 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-export * from './debounce'
-export * from './params'
+export * from "./debounce";
+export * from "./params";

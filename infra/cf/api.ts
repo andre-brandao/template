b3ecf2d47eb7ed60f80f78e0a7b060ef1f7a875f
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-07-17 22:42:04 -0300

    fix?

--- infra/cf/api.ts
+++ infra/cf/api.ts
@@ -8,9 +8,9 @@ const api = new sst.cloudflare.Worker("Api", {
   url: true,
   environment,
   link: [database, hyperdrive],
-  placement: {
-    region: "aws:sa-east-1"
-  },
+  // placement: {
+  //   region: "aws:sa-east-1"
+  // },
   transform: {
     worker: (args) => {
       args.compatibilityDate = "2026-03-19";
